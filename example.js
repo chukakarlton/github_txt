c293416968cceba8570b98d9cf6a7ff8ad146a2e
chukakarl <chukakarl1@gmail.com>
2024-03-17 13:32:46 +0100

    example.js

--- example.js
+++ example.js
@@ -1,1 +1,16 @@
 console.log('hello chuka')//my dear
+console.log('life isnt easy')
+console.log('but with determination')
+console.log('and persistence')
+console.log('we will conquer')
+let wholeNumber = 42; // initializing a variable
+const sayHi = function() {
+    console.log("Hi!");
+  };
+console.log(sayHi)  
+function fourScoreAndSevenYearsAgo(a,b) { // defining a function
+    // do something
+    let multiply = a * b;
+    console.log(multiply)
+  }
+      
