09b32222d793b02669ca02c39989248b9086e4b8
chukakarl <chukakarl1@gmail.com>
2024-03-19 15:27:25 +0100

    example.js

--- example.js
+++ example.js
@@ -1,4 +1,4 @@
-console.log('hello chuka')//my dear
+/*console.log('hello chuka')//my dear
 console.log('life isnt easy')
 console.log('but with determination')
 console.log('and persistence')
@@ -11,6 +11,8 @@ console.log(sayHi)
 function fourScoreAndSevenYearsAgo(a,b) { // defining a function
     // do something
     let multiply = a * b;
-    console.log(multiply)
-  }
-      
+    console.log(multiply)//
+  }*/
+  //> `5 plus 5 equals ${5 + 5}`//
+  let ada = `5 plus 5 equals ${5 + 5}`
+  console.log(ada)
